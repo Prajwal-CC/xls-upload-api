5d79f1cebc62b869e99a95429715e042e5db6991
Prajwal-CC <prajwal.conceptivecommerce@gmail.com>
2023-08-28 15:19:22 +0530

    changes done

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
-const app = express();
+const multer = require('multer');
+const xlsx = require('xlsx');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // res.render('index', { title: 'Member List', results: [], messages: { success: true } });
-  res.render('index')
+  res.render('index', { title: 'Member List', results: [], messages: { success: true } });
+});
+// router.post('/', function (req, res) { res.send("done here") })
+router.post('/upload', upload.single('file'), (req, res) => {
+
+  const fileBuffer = req.file.buffer;
+  const workbook = xlsx.read(fileBuffer, { type: 'buffer' });
+  const sheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[sheetName];
+  const jsonData = xlsx.utils.sheet_to_json(sheet);
+  // res.json(jsonData);
+  res.render('index', { title: 'Member List', results: jsonData, messages: { success: true } });
 });
 
-app.set('view engine', 'ejs');
 
 module.exports = router;
